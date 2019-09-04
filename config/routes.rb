bf102ff14c8aedea3e9535b33f1afbebf327544f
Wendy Calderon <wendycalderon@gmail.com>
2019-09-04 01:49:30 -0400

    Fix typo

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # schoolsearchny.com/api/v1/resource
-  namespace: api do
-    namespace: v1 do
+  namespace :api do
+    namespace :v1 do
       resources :users
     end
   end
