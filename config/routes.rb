8082c2050d18e85087c11ce89a6a556ce99506ac
Wendy Calderon <wendycalderon@gmail.com>
2019-09-04 01:41:02 -0400

    Nest users resources in api/v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  resources :users
+  # schoolsearchny.com/api/v1/resource
+  namespace: api do
+    namespace: v1 do
+      resources :users
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
