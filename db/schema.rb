31f57b72ebc9cd24be5713a88f1dc08868f6ae0c
Wendy Calderon <wendycalderon@gmail.com>
2019-09-05 11:39:23 -0400

    Run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_05_010753) do
+ActiveRecord::Schema.define(version: 2019_09_05_143525) do
+
+  create_table "bookmarks", force: :cascade do |t|
+    t.string "user_id"
+    t.string "school_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "schools", force: :cascade do |t|
     t.string "name"
