3713bebba1d0b8f89a17094e0d1280a6ca47da80
Wendy Calderon <wendycalderon@gmail.com>
2019-09-04 21:16:02 -0400

    Nest school resources under api/v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :schools
-  # schoolsearchny.com/api/v1/resource
+  # findmyschool.com/api/v1/resource
   namespace :api do
     namespace :v1 do
       resources :users
+      resources :schools
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
