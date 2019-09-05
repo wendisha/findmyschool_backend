2d54b78845ca90b696c4df18dbe3fb8ae9624cef
Wendy Calderon <wendycalderon@gmail.com>
2019-09-04 20:54:19 -0400

    Add name to users - reseed db

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_04_053032) do
+ActiveRecord::Schema.define(version: 2019_09_05_005208) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
 end
