095d0f8bfb8efe421f4d076d5604e9537f5b1848
Wendy Calderon <wendycalderon@gmail.com>
2019-09-05 14:19:48 -0400

    Add associations

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,2 +1,4 @@
 class School < ApplicationRecord
+    has_many :bookmarks
+    has_many :users, through: :bookmarks
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
     #Allow bcrypt to hash our passwords
-    has_secure_password 
+    has_secure_password
+    has_many :bookmarks
+    has_many :schools, through: :bookmarks
 end
