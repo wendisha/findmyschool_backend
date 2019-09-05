a570fff79526a0ac5ff4e5362e2e69bb684de4b6
Wendy Calderon <wendycalderon@gmail.com>
2019-09-05 16:52:14 -0400

    Nest bookmark resources under api/v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  resources :bookmarks
   # findmyschool.com/api/v1/resource
   namespace :api do
     namespace :v1 do
       resources :users
       resources :schools
+      resources :bookmarks
     end
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
