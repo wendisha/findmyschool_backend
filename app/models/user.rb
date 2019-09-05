00607f9259b35799c909fbc33a8a5380cdeed054
Wendy Calderon <wendycalderon@gmail.com>
2019-09-05 17:28:29 -0400

    Add foreing key to try to show bookmarks on users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     #Allow bcrypt to hash our passwords
     has_secure_password
-    has_many :bookmarks
+    has_many :bookmarks, foreign_key: :user_id, class_name: "Bookmark"
     has_many :schools, through: :bookmarks
 end
