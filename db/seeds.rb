b3c46819e1f9e3f2079f529508bc89eaef31e131
Wendy Calderon <wendycalderon@gmail.com>
2019-09-06 18:56:35 -0400

    Seed a new user

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,4 +10,6 @@
 
 # School.create(name: "Ps Ms 200 The Magnet School of Global Studies & Leadership", school_type: "Ppublic", district_id: 25, grade_range: "PK-8", address: "70-10 164th Street, Queens, NY 11365", website: "https://sites.google.com/globalstudiesandleadership.org/msgsl/home", rating_info: "some info!")
 
-Bookmark.create(user_id: 1, school_id: 1)
+# Bookmark.create(user_id: 1, school_id: 1)
+
+User.create(username: "luna", password: "password", name: "Luna")
