ca64ac0b763e4f7d01c2b41c3da1eacd4d85e98d
Wendy Calderon <wendycalderon@gmail.com>
2019-09-05 22:54:36 -0400

    Add current_user and logged_in? methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::API
     include ActionController::MimeResponds
+
+    def current_user
+        User.find_by(id: session[:user_id])
+    end
+    
+    def logged_in?
+        !!current_user
+    end
 end
