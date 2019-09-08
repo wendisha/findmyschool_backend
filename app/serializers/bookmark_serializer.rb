b797b639abc97c2b5544b7ec2ebf5d36af158da9
Wendy Calderon <wendycalderon@gmail.com>
2019-09-08 17:00:22 -0400

    Comment out unnecessary code

--- app/serializers/bookmark_serializer.rb
+++ app/serializers/bookmark_serializer.rb
@@ -3,7 +3,7 @@ class BookmarkSerializer
   belongs_to :user, serializer: UserSerializer 
   belongs_to :school 
   attributes :user_id, :school_id
-  attribute :school_name do |bookmark|
-    school_name: bookmark.school_id.name
-  end
+  # attribute :school_name do |bookmark|
+  #   school_name: bookmark.school_id.name
+  # end
 end
