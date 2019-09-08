195f40ebfb6c4f7ec91f62d2ec43a918bb10c46b
Wendy Calderon <wendycalderon@gmail.com>
2019-09-08 13:23:33 -0400

    Add bookmark serializer

---
+++ app/serializers/bookmark_serializer.rb
@@ -0,0 +1,9 @@
+class BookmarkSerializer
+  include FastJsonapi::ObjectSerializer
+  belongs_to :user, serializer: UserSerializer 
+  belongs_to :school 
+  attributes :user_id, :school_id
+  attribute :school_name do |bookmark|
+    school_name: bookmark.school_id.name
+  end
+end
