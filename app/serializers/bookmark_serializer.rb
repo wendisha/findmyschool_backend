6a701c7a87d9b7febd933b006f76d60d3622ce5e
Wendy Calderon <wendycalderon@gmail.com>
2019-09-08 22:35:27 -0400

    Comment out associations to avoid rendering it in json

--- app/serializers/bookmark_serializer.rb
+++ app/serializers/bookmark_serializer.rb
@@ -1,7 +1,7 @@
 class BookmarkSerializer
   include FastJsonapi::ObjectSerializer
-  belongs_to :user, serializer: UserSerializer 
-  belongs_to :school 
+  # belongs_to :user, serializer: UserSerializer 
+  # belongs_to :school 
   # attributes :user_id, :school_id
   attribute :school_name do |object|
     object.school.name
