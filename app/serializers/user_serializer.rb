4039f85d4514f275444cf2880bec0d69a91be257
Wendy Calderon <wendycalderon@gmail.com>
2019-09-08 22:29:27 -0400

    Add bookmarks attribute

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,4 +2,10 @@ class UserSerializer
   include FastJsonapi::ObjectSerializer
   attributes :username, :name
   has_many :bookmarks, serializer: BookmarkSerializer 
+  #reference: https://github.com/Netflix/fast_jsonapi/issues/160
+  attribute :bookmarks do |object|
+    BookmarkSerializer.new(object.bookmarks).as_json["data"]
+  end
 end
+
+
