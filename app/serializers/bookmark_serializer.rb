c24a8f3b02b0ed4aac0febd27711c56c739947d7
Wendy Calderon <wendycalderon@gmail.com>
2019-09-08 22:28:48 -0400

    Add school_name attribute

--- app/serializers/bookmark_serializer.rb
+++ app/serializers/bookmark_serializer.rb
@@ -2,7 +2,7 @@ class BookmarkSerializer
   include FastJsonapi::ObjectSerializer
   belongs_to :user, serializer: UserSerializer 
   belongs_to :school 
-  attributes :user_id, :school_id
+  # attributes :user_id, :school_id
   attribute :school_name do |object|
     object.school.name
   end
