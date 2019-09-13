0d646c5ffb28f6fff16ef927b8b5ae572e82a1ca
Wendy Calderon <wendycalderon@gmail.com>
2019-09-13 18:03:25 -0400

    Comment code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    #Allow bcrypt to hash our passwords
+    #Allow bcrypt to hash our passwords, validate passwords
     has_secure_password
     has_many :bookmarks #, foreign_key: :user_id, class_name: "Bookmark"
     has_many :schools, through: :bookmarks
