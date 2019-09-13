6309e10aba28792b5f856bcc279564581d7d0cf5
Wendy Calderon <wendycalderon@gmail.com>
2019-09-13 18:15:02 -0400

    Validate username and name

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
     has_secure_password
     has_many :bookmarks #, foreign_key: :user_id, class_name: "Bookmark"
     has_many :schools, through: :bookmarks
+
+    validates :name, :username, presence: true
 end
