a3bcbe45eb08a8370732894c4ce209739cf22509
Wendy Calderon <wendycalderon@gmail.com>
2019-09-13 23:44:26 -0400

    Modify create action render

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -19,8 +19,11 @@ class Api::V1::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    if @user.save
-      render json: @user, status: :created, location: @user
+    # if @user.save
+    #   render json: @user, status: :created
+      if @user.save
+        session[:user_id] = @user.id
+        render json: UserSerializer.new(@user), status: :created
     else
       resp = {
         error: @user.errors.full_messages.to_sentence
