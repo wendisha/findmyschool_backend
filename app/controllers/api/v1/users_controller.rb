cd7f87bf135b451f558093d1b869148db84fa731
Wendy Calderon <wendycalderon@gmail.com>
2019-09-13 23:31:26 -0400

    Add error message to create action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -22,7 +22,10 @@ class Api::V1::UsersController < ApplicationController
     if @user.save
       render json: @user, status: :created, location: @user
     else
-      render json: @user.errors, status: :unprocessable_entity
+      resp = {
+        error: @user.errors.full_messages.to_sentence
+      }
+      render json: resp, status: :unprocessable_entity
     end
   end
 
