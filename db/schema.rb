86ac1a20605044b885ce89e410c734b0d4658b92
Wendy Calderon <wendycalderon@gmail.com>
2019-09-21 10:17:21 -0400

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_20_213014) do
+ActiveRecord::Schema.define(version: 2019_09_21_141020) do
 
   create_table "bookmarks", force: :cascade do |t|
-    t.string "user_id"
-    t.string "school_id"
+    t.integer "user_id"
+    t.integer "school_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
